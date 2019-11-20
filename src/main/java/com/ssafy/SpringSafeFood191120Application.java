ab5af1c61907a8aa761b86e016764ee76738fd12
강민기 <kmandu94@gmail.com>
2019-11-20 13:15:39 +0900

    1

---
+++ src/main/java/com/ssafy/SpringSafeFood191120Application.java
@@ -0,0 +1,13 @@
+package com.ssafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSafeFood191120Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSafeFood191120Application.class, args);
+	}
+
+}
